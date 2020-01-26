04f7994984ecb597112fd5b88d26d902e8d70dc9
Louis Tsai <louis993546@gmail.com>
2020-01-26 10:33:30 +0100

    Initial Commit

---
+++ FitX/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FitX
+//
+//  Created by Louis Tsai on 1/26/20.
+//  Copyright © 2020 Louis Tsai. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
